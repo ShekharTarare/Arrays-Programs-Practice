b79db43c773d5d7244f2b111209761e13f3335b4
Shekhar Tarare <shekhartarare@gmail.com>
2022-01-03 02:58:25 +0530

    Added Kadane's algorithm and code for cyclically rotate by one

---
+++ CyclicallyRotateByOne/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyclicallyRotateByOne
+{
+    internal class Program
+    {
+        private static void CyclicallyRotate(int[] arr, int n)
+        {
+            int last = arr[arr.Length - 1];
+            int i = n - 1;
+            while(i > 0)
+            {
+                arr[i] = arr[i - 1];
+                i--;
+            }
+            arr[0] = last; 
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            CyclicallyRotate(arr, arr.Length);
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
---
+++ KadanesAlgorithm/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KadanesAlgorithm
+{
+    internal class Program
+    {
+        private static void KadaneAlgo(int[] arr, int n)
+        {
+            int sum = 0, max = arr[0];
+            for(int i = 0; i < n; i++)
+            {
+                sum += arr[i];
+                if (sum < 0)
+                    sum = 0;
+                if(max < sum)
+                    max = sum;
+            }
+            Console.WriteLine("The contiguous sub array which has maximum sum is " + max);
+        }
+
+        private static void KadaneAlgoHandleNegative(int[] arr, int n)
+        {
+            int sum = 0, max = arr[0];
+            for(int i = 0; i < n; i++)
+            {
+                sum += arr[i];
+                if (sum < arr[i]) //comparing the array element and storing it if it is lesser
+                    sum = arr[i];
+                if (max < sum)
+                    max = sum;
+            }
+            Console.WriteLine("The contiguous sub array which has maximum sum is " + max);
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 1, 2, 3, -2, 5 };
+            int[] arr2 = { -1, -2, -3, -4 };
+            KadaneAlgo(arr1, 5);
+            KadaneAlgoHandleNegative(arr2, 4);
+        }
+    }
+}
